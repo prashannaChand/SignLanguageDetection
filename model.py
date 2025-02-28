b99483fb4768543211b8fdf245dc6075afca7b12
Prashanna Chand <chandprashanna99@gmail.com>
2025-02-28 23:16:09 +0545

    preprocessing

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-# A model to read sign alphabets using opencv ko videorecording 
+#create a model to predict the sign language alphabet
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+
